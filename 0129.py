7623893cea0fd8c45bc05841f1e735da6208c3e7
isaaccantwin <154404612+isaaccantwin@users.noreply.github.com>
2026-01-29 16:11:42 +0800

    Update 0129.py

--- 0129.py
+++ 0129.py
@@ -9,10 +9,11 @@ def display_state(states):
     led_a.value(states[0])
     led_b.value(states[1])
     led_c.value(states[2])
-    
-a=int(input("a:1 or 0:"))
-b=int(input("b:1 or 0:"))
-c=int(input("c:1 or 0:"))
-out_c=(a&b)^c
-display_state((a,b,out_c))
-time.sleep(1.5)
+while True:
+   a=int(input("a:1 or 0:"))
+   b=int(input("b:1 or 0:"))
+   c=int(input("c:1 or 0:"))
+   out_c=(a&b)^c
+   display_state((a,b,out_c))
+   time.sleep(1.5)
+   print("next")
